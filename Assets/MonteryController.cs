1de903bc8b2249d5258ccbbbe7978f35336e2bd9
CodeWithPeakyBlinders×͜×☂ <namlhhe153319@fpt.edu.vn>
2023-03-05 14:52:27 +0700

    Create các thuộc tính của Monter,focus về  trụ thành

---
+++ Assets/MonteryController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonteryController : MonoBehaviour
+{
+    public float Speed = 2f;
+    public Transform target;
+    public int Health = 300;
+    public int damage = 10;
+    private int currentHealth;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        currentHealth = Health;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (target != null)
+        {
+            Vector2 direction = (target.position - transform.position).normalized;
+            rb.velocity = direction * Speed;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Tower"))
+        {
+            rb.velocity = Vector2.zero;
+            rb.AddForce((transform.position - collision.transform.position) * 500f);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        // Khi quái vật chết, xóa nó khỏi scene
+        Destroy(gameObject);
+    }
+}
