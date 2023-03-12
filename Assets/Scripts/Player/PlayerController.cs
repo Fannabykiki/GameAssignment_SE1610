3b4bb41d4b54ee29462fe101a278d5142edb8940
Trần Xuân Sơn <sowndev@gmail.com>
2023-03-12 23:25:20 +0700

    Add Sword + Attack Monsters

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,15 +7,25 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     private Rigidbody2D rb;
     private Animator ani;
-
-
+    public GameObject swordHitbox;
+    Collider2D swordCollider;
+    public Transform characterTransform;
+    public Transform swordTransform;
     private int playerHealth = 100;
+<<<<<<< Updated upstream
     public float speed;
     
     //move
+=======
+    public float speed = 5f;
+    GameObject player;
+    //moveW
+>>>>>>> Stashed changes
     private float left_right;
     private float up_down;
     private bool isfacingRight = true;
+    
+    public bool isAttacking = false;
     //attack
     public float attackRange;
     public int attackDamage;
@@ -25,19 +35,40 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         ani = GetComponent<Animator>();
+<<<<<<< Updated upstream
         
+=======
+        skillPrefab.SetActive(false);
+        skillPrefab2.SetActive(false);
+        swordCollider = swordHitbox.GetComponent<Collider2D>();
+        characterTransform = player.transform;
+        swordTransform = transform;
+>>>>>>> Stashed changes
     }
 
     // Update is called once per frame
     void Update()
 
     {
+        if (characterTransform.localScale.x > 0)
+        {
+            // nếu nhân vật quay sang phải, xoay gameobject kiếm về bên phải
+            swordTransform.localScale = new Vector3(1, 1, 1);
+        }
+        else
+        {
+            // nếu nhân vật quay sang trái, xoay gameobject kiếm về bên trái
+            swordTransform.localScale = new Vector3(-1, 1, 1);
+        }
         //move
-        left_right = Input.GetAxis("Horizontal");
-        up_down = Input.GetAxis("Vertical");
-        rb.velocity = new Vector2(left_right * speed, rb.velocity.y);
-        rb.velocity = new Vector2(rb.velocity.x, up_down * speed);
-
+        if (!isAttacking)
+        {
+            left_right = Input.GetAxis("Horizontal");
+            up_down = Input.GetAxis("Vertical");
+            rb.velocity = new Vector2(left_right * speed, rb.velocity.y);
+            rb.velocity = new Vector2(rb.velocity.x, up_down * speed);
+            
+        }
         //animation
         flip();
         ani.SetFloat("move", Mathf.Abs(left_right));
@@ -52,6 +83,8 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             ani.SetTrigger("attack");
+            isAttacking = true;
+            rb.velocity = Vector2.zero;
             //Attack();
         }
         
@@ -60,17 +93,25 @@ public class PlayerController : MonoBehaviour
 
     void flip()
     {
+        
         if(isfacingRight && left_right <0 || !isfacingRight && left_right > 0)
         {
             isfacingRight = !isfacingRight;
+            
             Vector3 scale = transform.localScale;
             scale.x = scale.x * -1;
             transform.localScale = scale;
+            
         }
     }
     private void OnCollisionStay2D(Collision2D collision)
     {
 
+    }
+
+    public void EndAttack()
+    {
+        isAttacking = false;
     }
     //void Attack()
     //{
