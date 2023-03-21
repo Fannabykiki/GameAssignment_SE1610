d4c08b782adb686ca94c6e5805dd60044a559ebc
Fannabykiki <namplhe153292@fpt.edu.vn>
2023-03-22 02:14:05 +0700

    Save and Load

---
+++ Assets/Scripts/DataPersistence/Data/GameData.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GameData 
+    
+{
+    public int currentHealth;
+    public int currentwave;
+    public int enemiesPerWave;
+    public int currentEnemies;
+    public int scoreValue;
+
+    // Start is called before the first frame update
+    public GameData()
+    {
+        this.currentHealth= 100;
+        this.currentwave= 0;
+        this.scoreValue= 0;
+        this.currentEnemies= 3;
+    }
+
+}
--- Assets/Scripts/Player/ScoreScript.cs
+++ Assets/Scripts/Player/ScoreScript.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ScoreScript : MonoBehaviour
+public class ScoreScript : MonoBehaviour,IDataPersistence
 {
     // Start is called before the first frame update
     public static int scoreValue = 0;
     Text score;
+
+    public void LoadData(GameData gameData)
+    {
+        scoreValue = gameData.scoreValue;
+    }
+
+    public void SaveData(ref GameData gameData)
+    {
+         gameData.scoreValue = scoreValue;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
