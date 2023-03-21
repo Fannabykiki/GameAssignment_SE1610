64bbcc317a4b7904bd0603757f1a3a0262ab1e32
CodeWithPeakyBlinders×͜×☂ <namlhhe153319@fpt.edu.vn>
2023-03-22 02:21:14 +0700

    update 3 monter

--- Assets/Scripts/Player/SwordHitbox.cs
+++ Assets/Scripts/Player/SwordHitbox.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SwordHitbox : MonoBehaviour
 {
     public int swordDamage = 1;
+    public float knockbackForce = 500f;
     public Collider2D swordCollider;
    
     // Start is called before the first frame update
@@ -25,7 +27,20 @@ public class SwordHitbox : MonoBehaviour
     }
     void OnCollisionEnter2D(Collision2D col)
     {
-        col.gameObject.SendMessage("OnHit", swordDamage);
+        IDamageable damageableObject = col.gameObject.GetComponent<IDamageable>();
+        if(damageableObject != null)
+        { //knockback
+            Vector3 parenPosition = gameObject.GetComponentInParent<Transform>().position;
+            Vector2 direction = (Vector2)(  col.gameObject.transform.position - parenPosition).normalized;
+            Vector2 knockback = direction * knockbackForce;
+            //col.gameObject.SendMessage("OnHit", swordDamage, knockback);
+            damageableObject.OnHit(swordDamage, knockback);
+        }
+        else
+        {
+            Debug.LogWarning("col does not implement IDamageable");
+        }
+        
     }
     
     
